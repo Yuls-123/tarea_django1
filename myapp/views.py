54371d265accfff14059341523c0af78fb2e681a
Yuls-123 <yap28092005@gmail.com>
2025-09-08 09:47:32 -0500

    deber

---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hello (request):
+    return HttpResponse("<h1>hello world</h1>")
+
+def nombre(request):
+    return HttpResponse("<h1>Yulady Ambas</h1>")
+
